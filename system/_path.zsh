ce286ef4fb465abc5ecca8fd63f87df4ac0438ad
lgraubner <graubnla@gmail.com>
2015-11-26 22:34:01 +0100

    fixed broken path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.node/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/.node/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
