74d3276a45c779976fe4a5d9877280c83d567512
lgraubner <graubnla@gmail.com>
2015-11-26 22:19:46 +0100

    fixed variable

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,1 +1,1 @@
-for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+for topic_folder ($DOTFILES/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
