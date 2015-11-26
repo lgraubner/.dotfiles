644071aba9e284c88fcce42cccf3498464d11c25
lgraubner <graubnla@gmail.com>
2015-11-26 21:37:44 +0100

    added npm aliases and packages

---
+++ npm/aliases.zsh
@@ -0,0 +1,4 @@
+# list global packages
+alias npm_lg="npm list --depth=0 -g"
+# list outdated global packages
+alias npm_og="npm outdated --depth=0 -g"
---
+++ npm/install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# NPM
+#
+# Installs usefull npm packages.
+
+source $HOME/.dotfiles/scripts/utils.sh
+
+# Check for NPM
+if [ ! $(which npm) ]; then
+  _running "Updating NPM"
+  echo ''
+
+  npm install -g npm
+
+  echo ''
+  _success "Updated NPM successfully"
+  _running "Installing dependencies"
+
+  echo ''
+  npm install -g bower jshint sitemap-generator
+  echo ''
+
+  _success "Dependencies installed successfully"
+fi
+
+exit 0
