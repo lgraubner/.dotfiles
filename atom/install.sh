4d21279c64e27af2736aaa5ab04473126dadd421
lgraubner <graubnla@gmail.com>
2015-12-04 13:15:48 +0100

    made executable

--- atom/install.sh
+++ atom/install.sh
@@ -11,7 +11,6 @@ source $DOTFILES/scripts/utils.sh
 if [ $(which apm) ]; then
   e_header "Installing atom packages"
 
-  # update NPM
   apm install autocomplete-php color-picker docblockr emmet linter linter-jshint minimap travis-ci-status
 
   e_success "Packages installed successfully"
--- atom/update.sh
+++ atom/update.sh
@@ -7,11 +7,14 @@
 source $DOTFILES/scripts/utils.sh
 
 if [ $(which apm) ]; then
-  e_header "Updating atom packages"
+    e_header "Updating atom packages"
 
-  apm update
-
-  e_success "Packages updated successfully"
+    for dir in $(find ~/.atom/packages -type d -maxdepth 1 -mindepth 1); do cd "$dir"
+            e_arrow "Updating plugin: $(basename $PWD)"
+            apm upgrade -c false
+    done
+    
+    e_success "Packages updated successfully"
 fi
 
 exit 0
