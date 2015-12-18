07ce143af774a0a7ee6f70a0176889abfa4b3869
lgraubner <graubnla@gmail.com>
2015-12-18 18:57:45 +0100

    added atom packages

--- atom/install.sh
+++ atom/install.sh
@@ -11,7 +11,7 @@ source $DOTFILES/scripts/utils.sh
 if [ $(which apm) ]; then
   e_header "Installing atom packages"
 
-  apm install autocomplete-php color-picker docblockr emmet jshint minimap react travis-ci-status
+  apm install autocomplete-php color-picker docblockr emmet jshint minimap react travis-ci-status pigments merge-conflicts atom-beautify
 
   e_success "Packages installed successfully"
 fi
