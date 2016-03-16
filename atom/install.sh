a2ada72fe97e8d4ba68944841b0fc2ec5dea8921
Lars Graubner <lg@leadfisher.com>
2016-03-16 17:28:31 +0100

    added javascript autocomplete, fixed php autocomplete

--- atom/install.sh
+++ atom/install.sh
@@ -11,7 +11,7 @@ source $DOTFILES/scripts/utils.sh
 if [ $(which apm) ]; then
   e_header "Installing atom packages"
 
-  apm install color-picker docblockr emmet linter-eslint minimap react pigments
+  apm install color-picker docblockr emmet linter-eslint minimap react pigments autocomplete-php atom-ternjs
 
   e_success "Packages installed successfully"
 fi
