81d27aa9dd65f2e106d4a5ac8eabfc3c6b3d0e80
Lars Graubner <lg@leadfisher.com>
2016-04-03 12:39:36 +0200

    added permission reset for node update

--- homebrew/update.sh
+++ homebrew/update.sh
@@ -8,6 +8,10 @@ source $DOTFILES/scripts/utils.sh
 
 if [[ $(which brew) ]]; then
     e_header "Updating Homebrew and upgrading formulaes"
+
+    # reset permissions for node
+    sudo chown -R $(whoami) /usr/local
+
     brew update
     brew upgrade --all
     brew cleanup
