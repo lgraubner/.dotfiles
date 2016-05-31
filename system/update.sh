996e109885f33c01c0bf99212fdb03c2feb8aa48
Lars Graubner <lg@leadfisher.com>
2016-05-31 10:15:57 +0200

    adjusted udpate messages

--- system/update.sh
+++ system/update.sh
@@ -6,13 +6,17 @@
 
 source $DOTFILES/scripts/utils.sh
 
-e_header "Updating global npm packages"
+e_header "Updating nvm"
 
 # update nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+
+e_success "Updated nvm successfully"
+
+e_header "Updating composer itself"
 # update composer
 composer self-update
 
-e_success "Updated nvm successfully"
+e_success "Updated composer successfully"
 
 exit 0
