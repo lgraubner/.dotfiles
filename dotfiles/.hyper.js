03173271e142e51f0349130d7114370d4422817e
Lars Graubner <mail@larsgraubner.de>
2017-01-18 09:56:27 +0100

    increase hyper fontsize

--- dotfiles/.hyper.js
+++ dotfiles/.hyper.js
@@ -1,6 +1,6 @@
 module.exports = {
   config: {
-    fontSize: 14.5,
+    fontSize: 15,
     fontFamily: 'Inconsolata, Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
     termCSS: '::selection { background-color: rgba(233, 44, 108, 0.8); color: #fcfcfc; }',
     padding: '12px 14px',
