3dc1e561d476216d25cefaec1402843f45d76ac7
Lars Graubner <mail@larsgraubner.de>
2017-04-02 13:37:49 +0200

    adjust ansible version

--- lib/install.sh
+++ lib/install.sh
@@ -15,7 +15,7 @@ e_header "Install homebrew"
 
 # install brew formulaes
 e_header "Install brew formulaes"
-brew install grc coreutils spark node ansible tree python3 ruby openssl
+brew install grc coreutils spark node ansible@2.0 tree python3 ruby openssl
 
 # install yarn
 e_header "Install yarn"
