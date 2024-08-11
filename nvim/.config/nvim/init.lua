192fceb0f8509d02a0e32c2aa96632751550e48a
Lars Graubner <mail@larsgraubner.de>
2024-08-11 12:02:06 +0200

    feat: fix pnpm add astro formatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -689,6 +689,7 @@ require('lazy').setup({
         typescriptreact = { { 'prettierd', 'prettier' } },
         yaml = { { 'prettierd', 'prettier' } },
         go = { 'gofmt' },
+        astro = { { 'prettierd', 'prettier' } },
       },
     },
   },
@@ -898,7 +899,7 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
