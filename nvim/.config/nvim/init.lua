66f39f1b2d81dc689daa216f8149b0cec29e611b
Lars Graubner <larsgraubner@MacBook-Air-von-Lars.local>
2025-01-13 20:54:48 +0100

    feat: switch to ghostty, cleanup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -867,7 +867,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-macchiato'
+      vim.cmd.colorscheme 'catppuccin-mocha'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
