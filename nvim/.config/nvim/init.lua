db2e9ec13fc40598a60cb0dcd94c6c559b8089f3
= <mail@larsgraubner.de>
2025-06-22 00:44:29 +0200

    feat: disable italic

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -976,7 +976,9 @@ require('lazy').setup({
     name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      require('catppuccin').setup {}
+      require('catppuccin').setup {
+        no_italic = true,
+      }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
